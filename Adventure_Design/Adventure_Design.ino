7b3fe9044caddfed4019bdddef36cb62077b9160
Unique3279 <goomingi@gmail.com>
2024-11-27 11:39:19 +0900

    add string split method

--- Adventure_Design/Adventure_Design.ino
+++ Adventure_Design/Adventure_Design.ino
@@ -1,3 +1,34 @@
+String err[2];
+
+class str{
+public:
+  String S;
+
+  void getStr(String s){
+    s = self->S;
+  }
+
+  void split(String data, char sep){
+    int nCount = 0;
+    int nIndex = 0;
+
+    String temp = "";
+    String sData = data;
+    while(1){
+      nIndex = sData.indexOf(sep);
+
+      if(nIndex != -1){
+        temp = sData.substring(0, nIndex);
+        err[0] = temp;
+        sData = sData.substring(nIndex+1);
+      }
+      else{
+        err[1] = sData;
+      }
+    }
+  }
+}
+
 void setup(){
     Serial1.begin(9600);
     Serial1.setTimeout(20);
@@ -7,7 +38,5 @@ void setup(){
 }
 
 void loop(){
-  if(Serial1.available()){
-    String data = Serial1.readString();
-  }
+  
 }
